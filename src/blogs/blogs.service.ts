3271478faa3ff648b4392e3d7cce3ae8ee52bf6e
Vikas Yadav2 <vikas.yadav2@olacabs.com>
2022-08-08 17:59:11 +0530

    data created and fetched

---
+++ src/blogs/blogs.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@nestjs/common';
+
+// interface Blog{
+//     name:string,
+//     id:number
+// }
+
+
+@Injectable()
+export class BlogsService {
+    private readonly blogs:any[]=[];
+
+    create(data:any){
+        this.blogs.push(data)
+    }
+
+    finddata():any[]{
+    return this.blogs
+    }
+}
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,10 @@
 import { Body,Controller,Get, HttpCode, Param, Post, Query, Redirect } from '@nestjs/common';
 
+import { BlogsService } from 'src/blogs/blogs.service';
 @Controller('users')
 export class UsersController {
+
+    constructor(private blogService :BlogsService) {}
 
     @Get()
     userInfo():string{
@@ -15,13 +18,42 @@ export class UsersController {
     
     @Post("add-user")
     addUser(@Body() record:any):string{
-        console.log(record,"===")
+        // console.log(record,"===")
         return "OK ADD USER"
     }
 
     @Get("list/:id")
+    // @HttpCode(204)
     listUser(@Param() record:any):string{
-        console.log(record,"===")
-        return "List user"+record.id
+        // console.log(record,"===")
+        return "List user"+record.id 
     }
+
+    @Get("list")
+    // @HttpCode(204)
+    listFilterUser(@Query() record:any):string{
+         console.log(record,"===")
+        return "List Query user"+record.id 
+    }
+
+    @Get("version*card")
+    detailPage():string{
+        return "detail"
+    }
+
+    detailPages():string{
+        return "Default"
+    }
+
+    @Get("blog-list")
+    async blogList():Promise<any[]>{
+        return this.blogService.finddata()
+    }
+
+    @Post("blog-add")
+    blogList1(@Body() record:any){
+        return this.blogService.create(record)
+    }
+
+     
 }
